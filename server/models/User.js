361e67b4a9ced3506ae364c2d1ec1b3aa58c5cbc
Aditya Dewan <adityadewan22@gmail.com>
2025-08-11 23:56:17 +0530

    added bcrypt for authorization and hashing

--- server/models/User.js
+++ server/models/User.js
@@ -18,4 +18,15 @@ const user=mongoose.Schema({
 {timestamps:true}
 );
 
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+userSchema.methods.matchPassword=async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword,this.password);
+};
+
 export default mongoose.model("User",userSchema);
