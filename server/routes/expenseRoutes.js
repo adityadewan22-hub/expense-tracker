8414279dd2f835830afedb89603173c2a8033c20
Aditya Dewan <adityadewan22@gmail.com>
2025-08-15 22:59:39 +0530

    added delete route

--- server/routes/expenseRoutes.js
+++ server/routes/expenseRoutes.js
@@ -25,7 +25,37 @@ router.get('/',async(req,res)=>{
     }
 });
 router.patch('/',async(req,res)=>{
+    try{
+        const{_id,...updateData}=req.body;
+        if(!_id){
+            return res.status(400).json({message:"ID is required"});
+        }
+        const expenseChange=await Expense.findByIdAndUpdate(_id, updateData, {new:true});
+        if(!expenseChange){
+            return res.status(404).json({message:"expense not found"});
+        }
+        res.json(expenseChange);
+    }
+    catch(error){
+        res.status(500).json({error:error.message});
+    }
     
+});
+router.delete('/',async(req,res)=>{
+    try{
+        const{_id}=req.params;
+        if(!_id){
+            return res.status(400).json({message:"ID is required"});
+        }
+        const expenseRemove= await Expense.findByIdAndDelete(_id);
+        if(!expenseRemove){
+            return res.status(404).json({message:"expense not found"});
+        }
+        res.status(200).json({message:"expense deleted successfully", expense:expenseRemove});
+    }
+    catch(error){
+        res.status(500).json({error:error.message});
+    }
 })
 
 export default router;
