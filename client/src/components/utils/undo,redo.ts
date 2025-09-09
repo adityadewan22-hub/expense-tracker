8c6282a0f7648632eef03f605b1a427f66263e25
Aditya Dewan <adityadewan22@gmail.com>
2025-09-10 01:05:58 +0530

    added undo and redo to client

---
+++ client/src/components/utils/undo,redo.ts
@@ -0,0 +1,59 @@
+import type { Expense } from "../ExpenseList";
+
+type ActionType="add"|"delete"|"edit";
+
+interface Action{
+    type:ActionType;
+    expense:Expense;
+}
+
+let undostack:Action[]=[];
+let redostack:Action[]=[];
+
+export function recordAction(action:Action){
+    undostack.push(action);
+    redostack=[];
+}
+
+export function undo(expenses:Expense[]):Expense[]{
+    if(undostack.length===0){
+        return expenses;
+    }
+    const action =undostack.pop();
+    if(!action){
+        return expenses;
+    }
+    redostack.push(action);
+    switch(action.type){
+        case "add":
+            return[...expenses,action.expense];
+        case "delete":
+            return expenses.filter(e=>e._id!==action.expense._id);
+        case "edit":
+            return expenses.map(e=>
+                e._id===action.expense._id?action.expense:e
+            );
+        default:
+            return expenses;
+    }
+}
+
+export function redo(expenses: Expense[]): Expense[] {
+  if (redostack.length === 0) return expenses;
+
+  const action = redostack.pop()!;
+  undostack.push(action);
+
+  switch (action.type) {
+    case "add":
+      return [...expenses, action.expense];
+    case "delete":
+      return expenses.filter(e => e._id !== action.expense._id);
+    case "edit":
+      return expenses.map(e =>
+        e._id === action.expense._id ? action.expense : e
+      );
+    default:
+      return expenses;
+  }
+}
